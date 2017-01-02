d9e45190cf0d0bc80c482faf518b78e934db1b98
Lachlan Donald <lachlan@ljd.cc>
2017-01-02 14:21:32 +1100

    Add build debugging

--- .buildkite/release.sh
+++ .buildkite/release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail
 
-VERSION="$(git describe --tags --candidates=1)"
+VERSION="$(git describe --tags --candidates=1 2>/dev/null || echo dev)"
 
 download_github_release() {
   wget -N https://github.com/c4milo/github-release/releases/download/v1.0.8/github-release_v1.0.8_linux_amd64.tar.gz
@@ -13,7 +13,7 @@ github_release() {
   ./github-release cardigann/cardigann "$version" "$BUILDKITE_COMMIT" "$(git cat-file -p "$version" | tail -n +6)" ""
 }
 
-make setup
+ls -al build/
 
 if [[ "$BUILDKITE_TAG" =~ ^v ]] ; then
   download_github_release
